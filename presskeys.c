e2eace3363ce26c9f2292de17a19e0fd0ea78b4d
jose-isac <joseisac@tutamail.com>
2025-06-21 02:56:04 -0300

    feat: arguments handling

--- presskeys.c
+++ presskeys.c
@@ -1,7 +1,13 @@
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+        errx(EXIT_FAILURE, "Few arguments, missing string");
+    else if (argc > 2)
+        errx(EXIT_FAILURE, "Too many arguments");
+
     return EXIT_SUCCESS;
 }
