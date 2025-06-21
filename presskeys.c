73a58ad9a86f9fd5d50e1bf17da0c0d845e0e5a5
jose-isac <joseisac@tutamail.com>
2025-06-21 03:04:50 -0300

    feat: loop through the string argument

--- presskeys.c
+++ presskeys.c
@@ -1,6 +1,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,16 @@ int main(int argc, char *argv[])
         errx(EXIT_FAILURE, "Few arguments, missing string");
     else if (argc > 2)
         errx(EXIT_FAILURE, "Too many arguments");
+
+    char c;
+    size_t ind;
+    size_t length;
+
+    length = strlen(argv[1]);
+
+    for (ind = 0; ind != length; ind++) {
+        c = argv[1][ind];    
+    }
 
     return EXIT_SUCCESS;
 }
